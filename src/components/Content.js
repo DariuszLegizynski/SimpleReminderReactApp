3ca382a8c6cfbfe8a8c96c71420d75887875b22e
DariuszLegizynski <d.legizynski@gmail.com>
2020-05-19 11:38:38 +0200

    Added a small button functionality just to refresh my memory on how it was done

---
+++ src/components/Content.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Content(){
+    const buttonText = "Click me!";
+    const buttonStyle = {backgroundColor: "blue", color: "white"};
+
+    return(
+        <div>
+            <label className="label" for="name">
+                Enter name:
+            </label>
+            <input id="name" type="text" placeholder="type your name in here" />
+            <button style={buttonStyle}>{buttonText}</button>
+            <p>this is some content</p>
+        </div>
+    )
+}
+
+export default Content;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div>
+            <header>
+                this is my header
+            </header>
+        </div>
+    )
+}
+
+export default Header;
